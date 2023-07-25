ba3fbd0d4fa86ca081751cbbc3c925874de1ddcd
Synt4x3rror <synt4x3rror@live.com>
2023-07-25 08:13:24 -0400

    [Add] calls to query records and store records in main.py [Add] script termination via sys.exit() if config file is not loaded

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import datetime
 import os
+import sys
 
+from helpers.DBHelper import DBHelper
 from helpers.FileHelper import FileHelper
 from helpers.KinoHelper import KinoHelper
 
@@ -9,13 +11,18 @@ def main():
     cwd = os.path.dirname(__file__)
 
     config = FileHelper.load_yaml(os.path.join(cwd, 'config.yaml'))
+    if not config:
+        sys.exit('Unable to load config file')
 
     #Create a KinoHelper instance
     game_helper_instance = KinoHelper(config)
 
     # Query latest results and filter records to be added to the database.
+    results = game_helper_instance.query_kino_results()
 
     # Store the queried values in sqlite3 database
+    sqlite_helper_instance = DBHelper(config)
+    sqlite_helper_instance.insert_records(results)
 
     print(f'\nScrip finished: {datetime.datetime.now().strftime("%Y-%m-%d %I:%M:%S %p")}')
 
