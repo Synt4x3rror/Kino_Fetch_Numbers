04d397b56c5c24f47fd2d19dc8924593cc4d043f
Synt4x3rror <synt4x3rror@live.com>
2023-07-25 08:11:00 -0400

    [Add] print statements [Add] query_kino_results function to query the results API, nd process the results obtained (WIP)
    [Add] sort_record function to sort ascending the winning numbers reported by the results API

--- helpers/KinoHelper.py
+++ helpers/KinoHelper.py
@@ -6,8 +6,9 @@ class KinoHelper:
     def __init__(self, config):
         self.config = config
 
-    def query_latest_records(self):
+    def __query_latest_records(self):
         try:
+            print('Querying results [...]')
             res = requests.get(f"{self.config['kino_service']['url']}/{self.config['kino_service']['number_game']}/{self.config['kino_service']['drawings_per_request']}")
             if(res.status_code and res.status_code == 200 and res.text):
                 return json.loads(res.text)
@@ -17,3 +18,20 @@ class KinoHelper:
             print('An error has occurred while attempting to fetch results form the Loteria Electronica Results API')
             print(repr(e))
             sys.exit('FETCH')
+
+    def query_kino_results(self):
+        # Query the latest records in the Loteria Electronica de Puerto Rico
+        res = self.__query_latest_records()
+
+        # Check last result on the sqlite database
+
+        # Remove the results from results obtained, if necessary
+
+        return res
+
+    @staticmethod
+    def sort_record(record_string):
+        lst_records = record_string.split('-')
+        lst_records.sort()
+
+        return '-'.join(lst_records)
