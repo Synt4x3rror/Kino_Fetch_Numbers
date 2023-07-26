97fa3ca628525f00cc5443f421b0a466bbe4d689
Synt4x3rror <synt4x3rror@live.com>
2023-07-25 20:39:56 -0400

    [Add] validation in main.py so only if results lenght is greater than 0, then method to add records to database shall be called.
    [Add] call to KinoHelper.query_kino_results function in order to query the results API and obtain records nor already in the database.

--- main.py
+++ main.py
@@ -14,15 +14,19 @@ def main():
     if not config:
         sys.exit('Unable to load config file')
 
-    #Create a KinoHelper instance
+    # Create a KinoHelper instance
     game_helper_instance = KinoHelper(config)
+    # Create a DBHelper instance
+    sqlite_helper_instance = DBHelper(config)
 
     # Query latest results and filter records to be added to the database.
-    results = game_helper_instance.query_kino_results()
+    results = game_helper_instance.query_kino_results(sqlite_helper_instance)
 
-    # Store the queried values in sqlite3 database
-    sqlite_helper_instance = DBHelper(config)
-    sqlite_helper_instance.insert_records(results)
+    if len(results) > 0:
+        # Store the queried values in sqlite3 database
+        sqlite_helper_instance.insert_records(results)
+    else:
+        print('No results to insert into the SQLite Database')
 
     print(f'\nScrip finished: {datetime.datetime.now().strftime("%Y-%m-%d %I:%M:%S %p")}')
 
