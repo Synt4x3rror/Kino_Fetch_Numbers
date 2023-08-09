0e1dc4e9b84205d1ff40daead4f141242fa7ada1
Synt4x3rror <synt4x3rror@live.com>
2023-08-09 19:58:58 -0400

    [Modify] query_kino_results() in order to call __query_latest_record() to obtain how many records should be fetched from results API.
    [Add] __get_result_qty_fetch function

--- helpers/KinoHelper.py
+++ helpers/KinoHelper.py
@@ -1,3 +1,5 @@
+import datetime
+
 import requests
 import sys
 import json
@@ -6,10 +8,10 @@ class KinoHelper:
     def __init__(self, config):
         self.config = config
 
-    def __query_latest_records(self):
+    def __query_latest_records(self, query_results_qty):
         try:
             print('Querying results [...]')
-            res = requests.get(f"{self.config['kino_service']['url']}/{self.config['kino_service']['number_game']}/{self.config['kino_service']['drawings_per_request']}")
+            res = requests.get(f"{self.config['kino_service']['url']}/{self.config['kino_service']['number_game']}/{query_results_qty}")
             if(res.status_code and res.status_code == 200 and res.text):
                 return json.loads(res.text)
             else:
@@ -20,8 +22,13 @@ class KinoHelper:
             sys.exit('FETCH')
 
     def query_kino_results(self, sqlite_helper_instance):
+        # Determine how many records should be queried from the results API
+        results_to_query = self.__get_result_qty_fetch(sqlite_helper_instance)
+        if(results_to_query <= 0):
+            sys.exit('Not enough time has passed between script runs')
+
         # Query the latest records in the Loteria Electronica de Puerto Rico
-        res = self.__query_latest_records()
+        res = self.__query_latest_records(results_to_query)
 
         # Check last result on the sqlite database
         last_id = sqlite_helper_instance.query_latest_result_id()
@@ -30,6 +37,13 @@ class KinoHelper:
         res = [x for x in res if x['electronicDrawingID'] > last_id]
 
         return res
+
+    def __get_result_qty_fetch(self, sqlite_helper_instance):
+        # Query the sqlite database to obtain last date of most recent result
+        last_date = sqlite_helper_instance.query_latest_result_date()
+        now = int(datetime.datetime.now().timestamp())
+        seconds_per_drawing = self.config['kino_service']['minutes_per_drawing'] * 60
+        return int((now-last_date)/seconds_per_drawing)
 
     @staticmethod
     def sort_record(record_string):
